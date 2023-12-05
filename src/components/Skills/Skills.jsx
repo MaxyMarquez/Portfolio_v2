3d82b53ad2df692fe014bc8f17e73583df77b99e
Maxy Marquez <maxy.marquez@hotmail.com>
2023-12-05 14:50:39 -0300

    -

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,12 +6,11 @@ const Skills = () => {
     const { t } = useTranslation();
     return (
         <>
-            <section id='section2' className=' mt-20'>
+            <section id='section2' className='pt-20'>
                 <h1 className='text-4xl lg:text-5xl text-slate-300 m-5 font-mono border-b-4 border-red-400 w-max'>
                     {t('skills.title')}
                 </h1>
                 <div className='flex flex-col bg-transparent p-5 gap-4' >
-                    <h1 className='font-mono text-2xl text-slate-400 border-b border-slate-400 w-max'>Frontend</h1>
                     <div className='flex flex-wrap gap-4'>
                         <span className='relative group bg-transparent'>
                             <Icon icon="vscode-icons:file-type-html" className='w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:rotate-2 duration-300' />
@@ -62,9 +61,6 @@ const Skills = () => {
                                 Sass: Syntactically Awesome Style Sheets
                             </span>
                         </span>
-                    </div>
-                    <h1 className='font-mono text-2xl text-slate-400 border-b border-slate-400 w-max'>Backend</h1>
-                    <div className='flex flex-wrap gap-4'>
                         <span className='relative group bg-transparent'>
                             <Icon icon="devicon:javascript" className='w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:-rotate-2 duration-300' />
                             <span
@@ -94,7 +90,28 @@ const Skills = () => {
                                 PostgreSQL
                             </span>
                         </span>
-                    </div>
+                        <span className='relative group bg-transparent'>
+                            <Icon icon="devicon:github" className='w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:-rotate-2 duration-300' />
+                            <span
+                                className="absolute hidden group-hover:flex -top-3 -translate-y-full w-max p-2 bg-gray-700 rounded-lg text-center text-white text-sm transform -translate-x-1/2 left-1/2 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                                Github
+                            </span>
+                        </span>
+                        <span className='relative group bg-transparent'>
+                            <Icon icon="logos:trello" className='w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:rotate-2 duration-300' />
+                            <span
+                                className="absolute hidden group-hover:flex -top-3 -translate-y-full w-max p-2 bg-gray-700 rounded-lg text-center text-white text-sm transform -translate-x-1/2 left-1/2 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                                Trello
+                            </span>
+                        </span>
+                        <span className='relative group bg-transparent'>
+                            <Icon icon="logos:taiga" className='w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:-rotate-2 duration-300' />
+                            <span
+                                className="absolute hidden group-hover:flex -top-3 -translate-y-full w-max p-2 bg-gray-700 rounded-lg text-center text-white text-sm transform -translate-x-1/2 left-1/2 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                                Taiga
+                            </span>
+                        </span>
+                    </div >
                 </div >
             </section >
         </>
