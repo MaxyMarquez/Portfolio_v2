b231e0d20627fe62696a8bc33927285e8cdd1385
MaxyMarquez <maxy.marquez@hotmail.com>
2024-06-04 18:54:55 -0300

    -

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -48,6 +48,7 @@ const Skills = () => {
                 React Native
               </span>
             </span>
+
             <span className="relative group bg-transparent">
               <Icon
                 icon="devicon:nextjs"
@@ -57,6 +58,17 @@ const Skills = () => {
                 Next.js
               </span>
             </span>
+
+            <span className="relative group bg-transparent">
+              <Icon
+                icon="logos:astro"
+                className="w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:rotate-2 duration-300"
+              />
+              <span className="absolute hidden group-hover:flex -top-3 -translate-y-full w-max p-2 bg-gray-700 rounded-lg text-center text-white text-sm transform -translate-x-1/2 left-1/2 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                Astro
+              </span>
+            </span>
+
             <span className="relative group bg-transparent">
               <Icon
                 icon="devicon:redux"
@@ -100,6 +112,16 @@ const Skills = () => {
               />
               <span className="absolute hidden group-hover:flex -top-3 -translate-y-full w-max p-2 bg-gray-700 rounded-lg text-center text-white text-sm transform -translate-x-1/2 left-1/2 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
                 JavaScript
+              </span>
+            </span>
+
+            <span className="relative group bg-transparent">
+              <Icon
+                icon="vscode-icons:file-type-typescript-official"
+                className="w-10 h-10 lg:w-20 lg:h-20 group-hover:scale-125 group-hover:-rotate-2 duration-300"
+              />
+              <span className="absolute hidden group-hover:flex -top-3 -translate-y-full w-max p-2 bg-gray-700 rounded-lg text-center text-white text-sm transform -translate-x-1/2 left-1/2 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                TypeScript
               </span>
             </span>
 
